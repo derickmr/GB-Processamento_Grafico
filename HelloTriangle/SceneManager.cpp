42bea1c46170fb34bfb3c9812d668f71d67173d3
derickmr <34775979+derickmr@users.noreply.github.com>
2019-06-11 23:02:06 -0300

    Add files via upload

---
+++ HelloTriangle/SceneManager.cpp
@@ -0,0 +1,252 @@
+#include "SceneManager.h"
+
+//static controllers for mouse and keyboard
+static bool keys[1024];
+static bool resized;
+static GLuint width, height;
+
+SceneManager::SceneManager()
+{
+}
+
+SceneManager::~SceneManager()
+{
+}
+
+void SceneManager::initialize(GLuint w, GLuint h)
+{
+	width = w;
+	height = h;
+	
+	// GLFW - GLEW - OPENGL general setup -- TODO: config file
+	initializeGraphics();
+
+}
+
+void SceneManager::initializeGraphics()
+{
+	// Init GLFW
+	glfwInit();
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	window = glfwCreateWindow(width, height, "Hello Transform", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+
+	//Setando a callback de redimensionamento da janela
+	glfwSetWindowSizeCallback(window, resize);
+	
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+
+	}
+
+	// Build and compile our shader program
+	addShader("../shaders/transformations.vs", "../shaders/transformations.frag");
+
+	//setup the scene -- LEMBRANDO QUE A DESCRIÇÃO DE UMA CENA PODE VIR DE ARQUIVO(S) DE 
+	// CONFIGURAÇÃO
+	setupScene();
+
+	resized = true; //para entrar no setup da câmera na 1a vez
+	
+	setupCamera2D();
+
+}
+
+void SceneManager::addShader(string vFilename, string fFilename)
+{
+	shader = new Shader (vFilename.c_str(), fFilename.c_str());
+}
+
+
+void SceneManager::key_callback(GLFWwindow * window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			keys[key] = false;
+	}
+}
+
+void SceneManager::resize(GLFWwindow * window, int w, int h)
+{
+	width = w;
+	height = h;
+	resized = true;
+
+	// Define the viewport dimensions
+	glViewport(0, 0, width, height);
+}
+
+
+void SceneManager::do_movement()
+{
+	if (keys[GLFW_KEY_ESCAPE])
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+}
+
+void SceneManager::render()
+{
+	// Clear the colorbuffer
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Render scene
+	shader->Use();
+
+	
+	if (resized) //se houve redimensionamento na janela, redefine a projection matrix
+	{
+		setupCamera2D();
+		resized = false;
+	}
+
+
+	//Desenhando 25 tiles aleatoriamente na tela
+
+	for (int i = 0; i < 25; i++)
+	{
+		glm::mat4 model = glm::mat4();
+		model = glm::translate(model, glm::vec3(rand()%800, rand()%600, 0.0));  //posX e posY da tela
+		tileset[rand()%2].draw(model);
+	}
+
+}
+
+void SceneManager::run()
+{
+	//GAME LOOP
+	while (!glfwWindowShouldClose(window))
+	{
+		
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+
+		//Update method(s)
+		do_movement();
+
+		//Render scene
+		render();
+		
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+}
+
+void SceneManager::finish()
+{
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+}
+
+
+void SceneManager::setupScene()
+{
+	TileIso tile;
+	tile.setShader(shader);
+	tile.setCor(0.1, 0.5, 0.1);
+	tile.inicializar(64, 128);
+	tileset.push_back(tile);
+
+	tile.setCor(0.5, 0.2, 0.1);
+	tile.inicializar(64, 128);
+	tileset.push_back(tile);
+}
+
+void SceneManager::setupCamera2D()
+{
+	glViewport(0, 0, width, height);
+
+	//corrigindo o aspecto
+	float ratio;
+	float xMin = 0.0, xMax = 800, yMin = 600, yMax = 0.0, zNear = -1.0, zFar = 1.0;
+	if (width >= height)
+	{
+		ratio = width / (float)height;
+		projection = glm::ortho(xMin*ratio, xMax*ratio, yMin, yMax, zNear, zFar);
+	}
+	else
+	{
+		ratio = height / (float)width;
+		projection = glm::ortho(xMin, xMax, yMin*ratio, yMax*ratio, zNear, zFar);
+	}
+
+	// Get their uniform location
+	GLint projLoc = glGetUniformLocation(shader->Program, "projection");
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+}
+
+void SceneManager::setupTexture()
+{
+
+	// load and create a texture 
+	// -------------------------
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+										   // set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	//unsigned char *data = SOIL_load_image("../textures/wall.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+	//unsigned char *data = stbi_load("../textures/mario.png", &width, &height, &nrChannels, 0);
+	unsigned char *data = stbi_load("../textures/tileset.png", &width, &height, &nrChannels, 0);
+	
+	cout << "Nro de canais: " << nrChannels << endl;
+	cout << "largura x altura: " << width << " x " << height << endl;
+
+	/*glm::vec3 cor(255, 255, 0);
+
+	for (int i = 0; i < width * height* nrChannels; i += 4)
+	{
+		unsigned char c = data[i] * 0.2125 + data[i + 1] * 0.7154 + data[i + 2] * 0.0721;
+		
+		if (c <= 127)
+		{
+			data[i] = 0;
+			data[i + 1] = 0;
+			data[i + 2] = 0;
+		}
+		else
+		{
+			data[i] = 255;
+			data[i + 1] = 255;
+			data[i + 2] = 255;
+		}
+	}
+*/
+
+	if (data)
+	{
+		//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+
+	glActiveTexture(GL_TEXTURE0);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
